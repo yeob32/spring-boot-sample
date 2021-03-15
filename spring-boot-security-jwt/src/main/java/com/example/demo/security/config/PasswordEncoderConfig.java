8822d88c748c2a913eecc822bce43ac1af1aa338
Kim Sangyeob <yeob32@gmail.com>
2021-03-16 01:05:07 +0900

    # changed BCryptPasswordEncoder => PasswordEncoderFactories

--- spring-boot-security-jwt/src/main/java/com/example/demo/security/config/PasswordEncoderConfig.java
+++ spring-boot-security-jwt/src/main/java/com/example/demo/security/config/PasswordEncoderConfig.java
@@ -2,7 +2,7 @@ package com.example.demo.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
@@ -10,6 +10,6 @@ public class PasswordEncoderConfig {
 
     @Bean
     protected PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }

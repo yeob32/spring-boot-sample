96c72613f7cd26b70f0a6373d396331a5281b3bc
Kim Sangyeob <yeob32@gmail.com>
2021-05-10 23:44:35 +0900

    # prerequisite mixed kotlin

---
+++ spring-boot-mysql/build.gradle.kts
@@ -0,0 +1,5 @@
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    runtimeOnly("mysql:mysql-connector-java")
+}

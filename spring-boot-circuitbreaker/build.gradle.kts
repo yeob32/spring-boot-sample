3f95a656731fefd8fb966d801537c4c87bc42cb8
Kim Sangyeob <yeob32@gmail.com>
2022-01-10 22:43:39 +0900

    주석 추가

--- spring-boot-circuitbreaker/build.gradle.kts
+++ spring-boot-circuitbreaker/build.gradle.kts
@@ -1,6 +1,11 @@
 extra["springCloudVersion"] = "2021.0.0"
 
 dependencies {
+	// https://github.com/resilience4j/resilience4j/issues/1151
+	// spring boot 2.3.4 에서는 아래와 같이 충돌 회피
+//	implementation("io.github.resilience4j:resilience4j-circuitbreaker:1.5.0")
+//	implementation("io.github.resilience4j:resilience4j-spring-boot2:${property("resilience4jVersion")}") // 1.7.1
+
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
